962f8e1ee4e686f0fc861e1098767895f9891500
Thomas Czachorowski <Thomas.Czachorowski@cdpipelinedevshop.com>
2022-08-24 08:49:51 -0400

    Initial commit

---
+++ src/main/java/StringEquality.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class StringEquality {
+
+    public static void main(String[] args) {
+
+        String king = "Moe";
+        boolean rock = king.equals("Moe");
+
+
+        if (rock==true){
+            System.out.println("You are the king of rock and roll");
+        }
+        else{
+            System.out.println("You are not the king");
+        }
+    }
+}
